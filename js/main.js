111e134cabd6579540db08fed1e4673154f48bdd
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-09-26 17:49:21 +0200

    Matrizea ongi imprimatzen da

--- js/main.js
+++ js/main.js
@@ -1,10 +1,63 @@
-import Pieza from "./objects/piece.js";
+import Pieza from './objects/piece.js';
+
+
+const x = 10;
+const y = 20;
+
 
 let keyName;
+ 
+
+let kuadro = [];
+for (let i = 0; i < y; i++) {
+    kuadro[i] = new Array(x).fill(0);
+}
+console.log(kuadro);
+
+let pie = new Pieza("I"); 
+
+
+
+
+
+impMatriz();
+
+
+
+
 document.addEventListener('keydown', (event) => {
     keyName = event.key;
-    console.log(`Tecla presionada: ${keyName}`);
+    console.log(`Sakatutako tekla: ${keyName}`);
+
+
+
+
+    if(keyName == "a"){
+        pie.rotatu_90();
+    }
+    if(keyName == "d"){
+        pie.rotatu90();
+    }
+
+    
 });
 
 
-pieza 
+function impMatriz() {
+    const container = document.getElementById("proba");
+    container.innerHTML = "";
+
+    for (let i = 0; i < kuadro.length; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row"); 
+
+        
+        for (let j = 0; j < kuadro[i].length; j++) {
+            const cell = document.createElement("span");
+            cell.classList.add("cell");
+            cell.textContent = kuadro[i][j]; 
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+}
