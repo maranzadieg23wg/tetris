255d6a1ae952203b5f2fb53afc47edb273e1e88b
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-09-26 16:50:14 +0200

    Detektatzen da zer idazten den
    Detektatu egiten da ze tekla ukitzen diren

---
+++ js/main.js
@@ -0,0 +1,10 @@
+import Pieza from "./objects/piece.js";
+
+let keyName;
+document.addEventListener('keydown', (event) => {
+    keyName = event.key;
+    console.log(`Tecla presionada: ${keyName}`);
+});
+
+
+pieza 
