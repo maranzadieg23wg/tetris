b1f7043ba788ea809afebe90f404b318f74c4efb
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-10-03 19:46:55 +0200

    Probabilitate fitxategia komentatuta

--- js/obekuntzak/probabilitate.js
+++ js/obekuntzak/probabilitate.js
@@ -4,30 +4,35 @@ class Probabilitateak{
 
     constructor(){
         this.erakutsi=0;
-        this.prezioa = 250;
+        this.prezioa = 250; //← Esartzen dugu zenbat izango den hasierako prezioa.
 
-        this.zenbatBalio();
+        this.zenbatBalio(); // ← Sortu bezain pronto, esaten diogu jartzeko zenbat balio duten objetu bakoitzek.
     }
 
 
 
+    // ↓ Lortzen dugu zenbat balio duen objetu bakoitzak, momentu honetan, zenbat pieza erosi diren arabera.
     getPrezioa(){
         return this.prezioa*(this.erakutsi+1);
     }
 
+    // ↓ Esaten digu zenbat kostatu egingo den pieza bat teorian
     getTeorialPrezioa(z){
         return this.prezioa*(z+1);
     }
 
+    // ↓ Erosi dugula bat esan nahi du
     erakutsiGehiago(){
         this.erakutsi++;
     }
 
+    // ↓ Reseteatu egin da, esan nahi du ez dela ezta bat erosi.
     erakutsiReset(){
         this.erakutsi =0;
     }
 
 
+    // ↓ Imprimatzen du irudi bakoitzaren azpian zenbat balio duen pieza bakoitzak
     zenbatBalio(){
         // H
         const containerH = document.getElementById("H");
@@ -58,6 +63,7 @@ class Probabilitateak{
         containerT.innerHTML = this.getTeorialPrezioa(6)+"P";
     }
 
+    // ↓ Erosten den bakoitzean, esaten du zein izango den probabilitatea pieza bat edo beste tokatzekoa.
     probabilitatea(lista){
 
         console.log(lista);
@@ -94,6 +100,8 @@ class Probabilitateak{
         
     }
 
+    // ↓ Begiratzen du listan pieza dagoen a la ez. Ez badako ateratzeko probabilitatea 0 izango da
+    // Bestela, egiten da 1/ pieza kopurua, eta hau ondoren borobiltzen da eta lortzen dugu bere eunekoa.
     probalitate(lista, pieza){
         if(lista.includes(pieza)){
             let z = 1/lista.length;
