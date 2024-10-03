32c40d0353f62686f63f1dc2e85833b282221110
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-10-03 18:19:06 +0200

    CAT 🐈🐈‍⬛😻

--- js/main.js
+++ js/main.js
@@ -555,5 +555,9 @@ document.addEventListener('keydown', (event) => {
             rotatu_90();
             impMatriz(); 
         }
+
+        if (keyName === "1") {
+            puntuak.createFlyingGif(); 
+        }
     }
 });
