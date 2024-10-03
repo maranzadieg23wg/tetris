e708cb01ab15720e680d546e4ef1cd3eb31ba1ca
Manex Aranzadi <maranzadieg23wg@ikzubirimanteo.com>
2024-10-03 20:36:02 +0200

    Elementuen ordena aldatuta
    Horain jarraitzen zein ordenetan agertzen den pantailan eta kodean

--- js/obekuntzak/fondoa.js
+++ js/obekuntzak/fondoa.js
@@ -2,13 +2,18 @@ class FondoaAl{
 
     constructor(){
         /*******************Fondoa aldatu*******************/
+        document.getElementById("fondo-Normal").addEventListener("click", this.fondo_Normal);
         document.getElementById("fondo-G").addEventListener("click", this.fondo_G);
         document.getElementById("fondo-B").addEventListener("click", this.fondo_B);
-        document.getElementById("fondo-Normal").addEventListener("click", this.fondo_Normal);
+        
     }
 
 
     
+    fondo_Normal(){
+        document.body.style.background = "linear-gradient(180deg, rgba(113,113,113,1) 30%, rgba(0,0,0,1) 100%)";
+        document.getElementById("menu").style.backgroundColor="#000000";
+    }
 
     fondo_G(){
         document.body.style.backgroundImage = "url('../../img/giga-chad.png')";
@@ -26,10 +31,15 @@ class FondoaAl{
         document.getElementById("menu").style.backgroundColor= "#3b3b3b";
     }
 
-    fondo_Normal(){
-        document.body.style.background = "linear-gradient(180deg, rgba(113,113,113,1) 30%, rgba(0,0,0,1) 100%)";
-        document.getElementById("menu").style.backgroundColor="#000000";
+    fondo_Perso(ur){
+        document.body.style.backgroundImage = "url("+ur+")";
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundPosition = "center";
+        document.body.style.backgroundRepeat = "no-repeat";
+        document.getElementById("menu").style.backgroundColor= "#000000";
     }
+
+    
 }
 
 export default FondoaAl;
